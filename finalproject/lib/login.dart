3889844601c4822b9fb3c288b47eacfd181c5e9d
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-01-11 13:30:31 +0100

    security check

---
+++ finalproject/lib/login.dart
@@ -0,0 +1,88 @@
+import 'package:finalproject/register.dart';
+import 'package:flutter/material.dart';
+
+class LogIn extends StatefulWidget {
+  @override
+  _LogInState createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+
+  TextEditingController emailController;
+  TextEditingController passwordController;
+
+ @override
+ void initState() {
+   super.initState();
+ }
+
+ @override
+ Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Log In Selector Games App'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(20,10,40,0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Enter your email',
+                  labelText: 'Email',
+                  icon: Icon(
+                    Icons.email,
+                    size: 27,
+                    color: Colors.blue,
+                  ),
+                ),
+              controller: emailController,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  labelText: 'Password',
+                  icon: Icon(
+                    Icons.screen_lock_portrait,
+                    size: 27,
+                    color: Colors.blue,
+                  ),
+                ),
+              controller: passwordController,
+              ),
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(60,15,0,0),
+                    child: FlatButton(
+                      child: Text('Register Now'),
+                      color: Colors.red,
+                      onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) {
+                              return RegisterPage(); 
+                            },
+                          ));
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(35,15,0,0),
+                    child: FlatButton(
+                      child: Text('Log In'),
+                      color: Colors.blue,
+                      onPressed: () {
+
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+ }
+}
--- finalproject/lib/register.dart
+++ finalproject/lib/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -7,30 +8,117 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
 
+    TextEditingController nameController;
+    TextEditingController emailController;
+    TextEditingController passwordController;
+    TextEditingController passwordController2;
+    TextEditingController codeController;
 
   @override
   Widget build(BuildContext context) {
-      TextEditingController nameController;
-    TextEditingController emailController;
-    TextEditingController passwordController;
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
         backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text('Register'),
         ),
         body: Padding(
           padding: const EdgeInsets.fromLTRB(20,10,40,0),
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: 'Enter your name for the application',
-              labelText: 'Name',
-              icon: Icon(
-                Icons.account_circle,
-                size: 27,
-                color: Colors.blue,
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your name for the application',
+                  labelText: 'Name',
+                  icon: Icon(
+                    Icons.account_circle,
+                    size: 27,
+                    color: Colors.blue,
+                  ),
+                ),
+                controller: nameController,
               ),
-            ),
-            controller: nameController,
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Enter your email',
+                  labelText: 'Email',
+                  icon: Icon(
+                    Icons.email,
+                    size: 27,
+                    color: Colors.blue,
+                  ),
+                ),
+                controller: emailController,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  labelText: 'Password',
+                  icon: Icon(
+                    Icons.screen_lock_portrait,
+                    size: 27,
+                    color: Colors.blue,
+                  ),
+                ),
+                controller: passwordController,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your password again',
+                  labelText: 'Repeat Password',
+                  icon: Icon(
+                    Icons.screen_lock_portrait,
+                    size: 27,
+                    color: Colors.blue,
+                  ),
+                ),
+                controller: passwordController2,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: FlatButton(
+                  child: Text('Register'),
+                  color: Colors.red,
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (context) => AlertDialog(
+                        title: Text('Security Check'),
+                        content: Column(
+                          children: <Widget>[
+                            Text('An email has been sent to your email, please enter the code below'),
+                            TextField(
+                              controller: codeController,
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                hintText: "Enter the code",
+                                labelText: 'Code'
+                              ),
+                            ),
+                          ],
+                        ),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text('ACCEPT'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          FlatButton(
+                            child: Text('CANCEL'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       );
